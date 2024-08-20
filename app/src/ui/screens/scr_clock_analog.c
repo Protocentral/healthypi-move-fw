5b7d2b781aca8f38a66b2279239715712e3219f0
Ashwin K Whitchurch <ashwin@protocentral.com>
2024-08-20 22:10:34 +0530

    Update scr_clock_analog.c

--- app/src/ui/screens/scr_clock_analog.c
+++ app/src/ui/screens/scr_clock_analog.c
@@ -30,8 +30,9 @@ void draw_scr_clock_analog(enum scroll_dir m_scroll_dir)
     meter_clock = lv_meter_create(scr_clock_analog);
     lv_obj_set_size(meter_clock, 390, 390);
     //lv_obj_set_style_opa(meter_clock, 50, LV_PART_MAIN);
-    lv_obj_set_style_bg_color(meter_clock, lv_color_black(), LV_PART_MAIN);
+    //lv_obj_set_style_bg_color(meter_clock, lv_color_black(), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(meter_clock, 0, LV_PART_MAIN);
+
     lv_obj_center(meter_clock);
 
     /*Create a scale for the minutes*/
@@ -43,15 +44,15 @@ void draw_scr_clock_analog(enum scroll_dir m_scroll_dir)
     /*Create another scale for the hours. It's only visual and contains only major ticks*/
     lv_meter_scale_t *scale_hour = lv_meter_add_scale(meter_clock);
     lv_meter_set_scale_ticks(meter_clock, scale_hour, 12, 0, 0, lv_color_white());           /*12 ticks*/
-    lv_meter_set_scale_major_ticks(meter_clock, scale_hour, 1, 2, 20, lv_color_white(), 10); /*Every tick is major*/
+    lv_meter_set_scale_major_ticks(meter_clock, scale_hour, 1, 2, 20, lv_color_white(), 14); /*Every tick is major*/
     lv_meter_set_scale_range(meter_clock, scale_hour, 1, 12, 330, 300);                      /*[1..12] values in an almost full circle*/
 
     //LV_IMG_DECLARE(img_hand)
-    LV_IMG_DECLARE(clock_long_hand)
+    //LV_IMG_DECLARE(clock_long_hand)
 
     /*Add a the hands from images*/
-    lv_meter_indicator_t *indic_min = lv_meter_add_needle_img(meter_clock, scale_min, &clock_long_hand, 5, 5);
-    lv_meter_indicator_t *indic_hour = lv_meter_add_needle_img(meter_clock, scale_min, &clock_long_hand, 5, 5);
+    lv_meter_indicator_t *indic_min = lv_meter_add_needle_line(meter_clock, scale_min, 2, lv_palette_main(LV_PALETTE_RED), -25); //lv_meter_add_needle_img(meter_clock, scale_min, &clock_long_hand, 5, 5);
+    lv_meter_indicator_t *indic_hour = lv_meter_add_needle_line(meter_clock, scale_min, 2, lv_palette_main(LV_PALETTE_GREY), -35);//lv_meter_add_needle_img(meter_clock, scale_min, &clock_long_hand, 5, 5);
 
     /*Create an animation to set the value*/
     lv_anim_t a;
@@ -59,15 +60,14 @@ void draw_scr_clock_analog(enum scroll_dir m_scroll_dir)
     lv_anim_set_exec_cb(&a, set_value);
     lv_anim_set_values(&a, 0, 60);
     lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
-    lv_anim_set_time(&a, 2000); /*2 sec for 1 turn of the minute hand (1 hour)*/
+    lv_anim_set_time(&a, 60000); /*2 sec for 1 turn of the minute hand (1 hour)*/
     lv_anim_set_var(&a, indic_min);
     lv_anim_start(&a);
 
-    /*lv_anim_set_var(&a, indic_hour);
-    lv_anim_set_time(&a, 24000); //24 sec for 1 turn of the hour hand
+    lv_anim_set_var(&a, indic_hour);
+    lv_anim_set_time(&a, 120000); //24 sec for 1 turn of the hour hand
     lv_anim_set_values(&a, 0, 60);
     lv_anim_start(&a);
-    */
 
     // lv_obj_add_event_cb(btn_hr_disp, scr_clock_small_hr_event_handler, LV_EVENT_ALL, NULL);
 
