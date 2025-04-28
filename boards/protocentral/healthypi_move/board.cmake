8d2967dced4b36d6caf5af52421289a9b9fa914b
Ashwin K Whitchurch <ashwin@protocentral.com>
2025-04-28 14:32:18 +0530

    Added nrfutil support files

--- boards/protocentral/healthypi_move/board.cmake
+++ boards/protocentral/healthypi_move/board.cmake
@@ -4,8 +4,9 @@ if(CONFIG_BOARD_HEALTHYPI_MOVE_NRF5340_CPUAPP_NS)
   set(TFM_PUBLIC_KEY_FORMAT "full")
 endif()
 
-if(CONFIG_BOARD_HEALTHYPI_MOVE_NRF5340_NRF5340_CPUAPP OR CONFIG_BOARD_HEALTHYPI_MOVE_NRF5340_CPUAPP_NS)
-board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+if(CONFIG_BOARD_HEALTHYPI_MOVE_NRF5340_CPUAPP OR CONFIG_BOARD_HEALTHYPI_MOVE_NRF5340_CPUAPP_NS)
+  board_runner_args(nrfutil "--ext-mem-config-file=${BOARD_DIR}/support/healthypi_move_qspi_nrfutil_config.json")
+  board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
 endif()
 
 if(CONFIG_TFM_FLASH_MERGED_BINARY)
@@ -13,11 +14,10 @@ if(CONFIG_TFM_FLASH_MERGED_BINARY)
 endif()
 
 if(CONFIG_BOARD_HEALTHYPI_MOVE_NRF5340_CPUNET)
-board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+  board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
 endif()
 
-board_runner_args(nrfjprog "--softreset")
-
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
