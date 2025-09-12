82b37ed013efe097d9f552d3a12a57190a0848e4
Ashwin K Whitchurch <ashwin@protocentral.com>
2025-09-12 13:52:01 +0530

    Change control fi_sensor on/off

--- app/src/hw_module.h
+++ app/src/hw_module.h
@@ -42,8 +42,8 @@ void hpi_bpt_abort(void);
 
 void hpi_hw_pmic_off(void);
 
-void hpi_hw_ldsw2_off(void);
-void hpi_hw_ldsw2_on(void);
+void hpi_hw_fi_sensor_off(void);
+void hpi_hw_fi_sensor_on(void);
 
 void hpi_pwr_display_sleep(void);
 void hpi_pwr_display_wake(void);
