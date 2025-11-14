b47847ea1a8de13a0ca04bfebfefae1d478ae033
SuchithraButra <suchithra@circuitects.com>
2025-11-14 12:51:55 +0530

    In progress : Add GSR Summary

--- app/src/hpi_zbus_channels.c
+++ app/src/hpi_zbus_channels.c
@@ -101,6 +101,15 @@ ZBUS_CHAN_DEFINE(ecg_stat_chan, /* Name */
                  ZBUS_MSG_INIT(0) /* Initial value {0} */
 );
 
+#if defined(CONFIG_HPI_GSR_SCREEN)
+// Live GSR status channel (elapsed/remaining time updates)
+ZBUS_CHAN_DEFINE(gsr_status_chan,
+                 struct hpi_gsr_status_t,
+                 NULL,
+                 NULL,
+                 ZBUS_OBSERVERS(disp_gsr_status_lis),
+                 ZBUS_MSG_INIT(0));
+#endif
 
 #if defined(CONFIG_HPI_GSR_STRESS_INDEX)
 ZBUS_CHAN_DEFINE(gsr_stress_chan, /* Name */
@@ -112,12 +121,4 @@ ZBUS_CHAN_DEFINE(gsr_stress_chan, /* Name */
 );
 #endif
 
-#if defined(CONFIG_HPI_GSR_SCREEN)
-// Live GSR status channel (elapsed/remaining time updates)
-ZBUS_CHAN_DEFINE(gsr_status_chan,
-                 struct hpi_gsr_status_t,
-                 NULL,
-                 NULL,
-                 ZBUS_OBSERVERS(disp_gsr_status_lis),
-                 ZBUS_MSG_INIT(0));
-#endif
+
