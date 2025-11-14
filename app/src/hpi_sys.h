2f793dfd7edb494eb8ce6a2b6b4b58297f35b5cd
SuchithraButra <suchithra@circuitects.com>
2025-11-14 12:29:35 +0530

    Added Prototypes related to get LF/HF ratio updated time

--- app/src/hpi_sys.h
+++ app/src/hpi_sys.h
@@ -43,8 +43,9 @@ int hpi_sys_get_last_bp_update(uint8_t *bp_sys_last_value, uint8_t *bp_dia_last_
 int hpi_sys_get_last_ecg_update(uint8_t *ecg_hr, int64_t *ecg_last_update_ts);
 int hpi_sys_get_last_steps_update(uint16_t *steps_last_value, int64_t *steps_last_update_ts);
 int hpi_sys_get_last_temp_update(uint16_t *temp_last_value_x100, int64_t *temp_last_update_ts);
-int hpi_sys_get_last_gsr_update(uint16_t *gsr_last_value, int64_t *gsr_last_update_ts);
 int hpi_sys_get_last_lf_hf_update(uint16_t *hrv_last_lf_hf_value, int64_t *hrv_last_lf_hf_update_ts);
+int hpi_sys_get_last_gsr_update(uint16_t *gsr_last_value, int64_t *gsr_last_update_ts);
+
 
 void hpi_sys_set_device_on_skin(bool on_skin);
 bool hpi_sys_get_device_on_skin(void);
