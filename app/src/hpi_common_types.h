c36cd0d88ef1e13655fb72155ba5b416b069cb92
Gudemma <gudemma@circuitects.com>
2025-11-21 19:42:30 +0530

    Not done anything

--- app/src/hpi_common_types.h
+++ app/src/hpi_common_types.h
@@ -42,6 +42,8 @@ HealthyPi specific common data types
 #define BPT_PPG_POINTS_PER_SAMPLE 32
 
 #define ECG_RECORD_BUFFER_SAMPLES 3840 // Full 30 seconds at 128 Hz (128*30) - 15.36KB total
+#define GSR_RECORD_BUFFER_SAMPLES 960 // 32Hz * 30s (similar to ECG)
+//#define GSR_RECORD_BUFFER_SAMPLES 7680  // 128 Hz * 60 sec
 
 enum hpi_ppg_status 
 {
