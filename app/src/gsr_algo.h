730cf537ab6a44c4a8a067c013d3c918b92ed6c4
Gudemma <gudemma@circuitects.com>
2025-11-22 11:31:51 +0530

    Added function prototype

--- app/src/gsr_algo.h
+++ app/src/gsr_algo.h
@@ -6,8 +6,10 @@
 #define SCR_THRESHOLD_DEFAULT 150    // Default threshold, adjust per calibration
 #define MIN_PEAK_GAP 10              // Minimum samples between SCR peaks (~0.3 sec at 32Hz)
 
+
 // Function to calculate SCR count from GSR buffer
-//int calculate_scr_count(int32_t *gsr_buffer, uint16_t sample_count);
 int calculate_scr_count(int32_t *gsr_buffer, int sample_count);
+//int calculate_scr_count(int32_t *gsr_buffer, uint16_t sample_count);
+
 
 #endif // GSR_ALGO_H
