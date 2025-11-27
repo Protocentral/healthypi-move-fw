d4b5a59d9ddaa7bb3e39df823447b78764df8547
SuchithraButra <suchithra@circuitects.com>
2025-11-27 17:52:58 +0530

    Added HRV Screen case

--- app/src/ui/hp_ui_common.c
+++ app/src/ui/hp_ui_common.c
@@ -528,9 +528,6 @@ void hpi_load_screen(int m_screen, enum scroll_dir m_scroll_dir)
     case SCR_ECG:
         draw_scr_ecg(m_scroll_dir);
         break;
-    case SCR_HRV:
-        draw_scr_hrv(m_scroll_dir);
-        break;
     case SCR_GSR:
 #if defined(CONFIG_HPI_GSR_SCREEN)
         draw_scr_gsr(m_scroll_dir);
