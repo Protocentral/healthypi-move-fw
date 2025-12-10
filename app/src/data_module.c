688c3e8d8d372bb2f21e78ff6e66b972527b2226
Gudemma <gudemma@circuitects.com>
2025-12-10 19:38:35 +0530

    Added semaphore for gsr buffer

--- app/src/data_module.c
+++ app/src/data_module.c
@@ -511,43 +511,101 @@ void data_thread(void)
                 }
             }
 
-        k_mutex_lock(&mutex_is_gsr_record_active, K_FOREVER);
-        if (is_gsr_record_active)
+         k_mutex_lock(&mutex_is_gsr_record_active, K_FOREVER);
+        // if (is_gsr_record_active)
+        // {
+        //     int samples_to_copy = bsample.bioz_num_samples;
+        //     int space_left = GSR_RECORD_BUFFER_SAMPLES - gsr_record_counter;
+
+        //     if (space_left <= 0)
+        //     {
+        //         LOG_INF("GSR buffer full - stopping recording");
+        //         k_mutex_unlock(&mutex_is_gsr_record_active);
+        //         hpi_data_set_gsr_record_active(false);
+        //     }
+        //     else
+        //     {
+        //         int copy_count = (samples_to_copy <= space_left) ? samples_to_copy : space_left;
+        //         memcpy(&gsr_record_buffer[gsr_record_counter],
+        //             bsample.bioz_samples,
+        //             copy_count * sizeof(int32_t));
+        //         gsr_record_counter += copy_count;
+
+        //         if (copy_count < samples_to_copy)
+        //         {
+        //             LOG_WRN("GSR buffer full mid-batch - collected %d samples, discarded %d",
+        //                     gsr_record_counter, samples_to_copy - copy_count);
+        //             k_mutex_unlock(&mutex_is_gsr_record_active);
+        //             hpi_data_set_gsr_record_active(false);
+        //         }
+        //         else
+        //         {
+        //             k_mutex_unlock(&mutex_is_gsr_record_active);
+        //         }
+        //     }
+        // }
+        // else
+        // {
+        //     k_mutex_unlock(&mutex_is_gsr_record_active);
+        // }
+        // k_mutex_lock(&mutex_is_gsr_record_active, K_FOREVER);
+
+        if (is_gsr_record_active == true)
         {
             int samples_to_copy = bsample.bioz_num_samples;
             int space_left = GSR_RECORD_BUFFER_SAMPLES - gsr_record_counter;
 
-            if (space_left <= 0)
+            // Defensive check: prevent overflow
+            if (gsr_record_counter >= GSR_RECORD_BUFFER_SAMPLES)
             {
-                LOG_INF("GSR buffer full - stopping recording");
+                LOG_ERR("GSR buffer overflow detected");
+                extern struct k_sem sem_gsr_complete;
+                k_sem_give(&sem_gsr_complete);
                 k_mutex_unlock(&mutex_is_gsr_record_active);
-                hpi_data_set_gsr_record_active(false);
+                continue;
+            }
+
+            if (samples_to_copy <= space_left)
+            {
+                memcpy(&gsr_record_buffer[gsr_record_counter],
+                    bsample.bioz_samples,
+                    samples_to_copy * sizeof(int32_t));
+
+                gsr_record_counter += samples_to_copy;
+
+                // Completed exactly full buffer
+                if (gsr_record_counter >= GSR_RECORD_BUFFER_SAMPLES)
+                {
+                    LOG_WRN("GSR buffer full - collected %d samples(30.0 seconds @ 32Hz)", gsr_record_counter);
+                    LOG_INF("Signaling GSR state machine to stop recording");
+
+                    extern struct k_sem sem_gsr_complete;
+                    k_sem_give(&sem_gsr_complete);
+                }
             }
             else
             {
-                int copy_count = (samples_to_copy <= space_left) ? samples_to_copy : space_left;
-                memcpy(&gsr_record_buffer[gsr_record_counter],
-                    bsample.bioz_samples,
-                    copy_count * sizeof(int32_t));
-                gsr_record_counter += copy_count;
+                // Copy what fits
+                if (space_left > 0)
+                {
+                    memcpy(&gsr_record_buffer[gsr_record_counter],
+                        bsample.bioz_samples,
+                        space_left * sizeof(int32_t));
 
-                if (copy_count < samples_to_copy)
-                {
-                    LOG_WRN("GSR buffer full mid-batch - collected %d samples, discarded %d",
-                            gsr_record_counter, samples_to_copy - copy_count);
-                    k_mutex_unlock(&mutex_is_gsr_record_active);
-                    hpi_data_set_gsr_record_active(false);
+                    gsr_record_counter += space_left;
                 }
-                else
-                {
-                    k_mutex_unlock(&mutex_is_gsr_record_active);
-                }
+
+             //   LOG_WRN("GSR buffer full mid-batch - dropped samples");
+                LOG_WRN("GSR buffer full mid-batch - collected %d samples, discarded %d",gsr_record_counter, samples_to_copy - space_left);
+                LOG_INF("Signaling GSR state machine to stop recording");
+
+                extern struct k_sem sem_gsr_complete;
+                k_sem_give(&sem_gsr_complete);
             }
         }
-        else
-        {
-            k_mutex_unlock(&mutex_is_gsr_record_active);
-        }
+
+        k_mutex_unlock(&mutex_is_gsr_record_active);
+
 
 #if defined(CONFIG_HPI_GSR_STRESS_INDEX)
             // Calculate stress index from GSR samples
